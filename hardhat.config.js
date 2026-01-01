98068411445eec5c63cc85e810089f11d88fc172
jamgohouse-ops <jamgohouse@gmail.com>
2026-01-01 18:45:39 -0500

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,10 @@
+import "@nomicfoundation/hardhat-toolbox";
+
+/** @type import('hardhat/config').HardhatUserConfig */
+const config = {
+  solidity: {
+    version: "0.8.20",
+  },
+};
+
+export default config;
